05cef47b059386f65fbcf5f9ee935c22adda12e5
Your Name <divyanshutyagi248@gmail.com>
2026-03-22 10:17:40 +0530

    Fix JAR name for Azure Deployement

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,10 @@ allOpen {
     annotation("jakarta.persistence.Embeddable")
 }
 
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveFileName.set("app.jar")
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
